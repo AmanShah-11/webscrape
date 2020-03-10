6bed00ed01d6ab825e0689458ddd1173a0282807
AmanShah-11 <59811053+AmanShah-11@users.noreply.github.com>
2020-03-10 16:27:57 -0400

    Add files via upload

---
+++ schema.py
@@ -0,0 +1,18 @@
+SCHEMA = [
+    'date',
+    'employee_title',
+    'location',
+    'employee_status',
+    'review_title',
+    'helpful',
+    'pros',
+    'cons',
+    'MainText',
+    'rating_overall',
+    'rating_balance',
+    'rating_culture',
+    'rating_career',
+    'rating_comp',
+    'rating_mgmt',
+
+]
